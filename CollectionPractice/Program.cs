97f7cdb592bac69802cf72e7d2323b69906a3149
Utthirabalaji <31762655+Utthirabalaji@users.noreply.github.com>
2018-10-09 15:02:35 +0530

    collection practice uploaded

---
+++ CollectionPractice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionPractice
+{
+   public class Program
+    {
+        public static String GetHashMap()
+        {
+            return "Hello World";
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetHashMap());
+            Console.Read();
+        }
+    }
+}
